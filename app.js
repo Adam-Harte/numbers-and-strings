7eb431832afec64452746d014d8d469e7d1572b0
Adam-Harte <adamharte11@hotmail.co.uk>
2020-04-05 21:25:02 +0100

    bigInt added

--- app.js
+++ app.js
@@ -9,3 +9,10 @@ console.log(0.2 + 0.4 === 0.8);
 console.log(0.2);
 console.log((0.2).toFixed(1));
 console.log(((0.2 + 0.4) * 100) / 100);
+
+// bigInt
+const bigInt1 = 56710000029843109982104n;
+const bigInt2 = bigInt(74710030029843109982191);
+
+console.log(bigInt1);
+console.log(bigInt2);
