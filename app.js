f21d08e1d4d828ab766c31144a236da0835788e1
Adam-Harte <adamharte11@hotmail.co.uk>
2020-04-05 19:40:46 +0100

    floating point precision added

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ console.log(Number.MAX_SAFE_INTEGER);
 console.log(Number.MAX_VALUE);
 console.log(Number.MIN_SAFE_INTEGER);
 console.log(Number.MIN_VALUE);
+
+// floating point precision
+console.log(0.2 + 0.4 === 0.8);
+console.log(0.2);
+console.log((0.2).toFixed(1));
+console.log(((0.2 + 0.4) * 100) / 100);
